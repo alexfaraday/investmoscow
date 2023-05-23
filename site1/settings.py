dfd1e27c7bbf4431d107d81b8092c04cb727f622
alexfaraday <like.alex.melnikov@gmail.com>
2023-05-23 09:20:33 +0300

    change mainpage/templates

--- site1/settings.py
+++ site1/settings.py
@@ -133,4 +133,4 @@ STATIC_ROOT = '/home/c/cp31594/django_gsvno/public_htmlstatic'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL='/'
+LOGIN_REDIRECT_URL='profile'
