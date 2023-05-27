7e2aaed4c78a09fefa9af4a29020d29fb343bfd1
alexfaraday <like.alex.melnikov@gmail.com>
2023-05-27 09:53:03 +0300

    correct models

--- mainpage/forms.py
+++ mainpage/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from .models import Profile, Branch, Order_Varibles
+from django.contrib.auth.models import User
 
 
 class ProfileForm(ModelForm):
