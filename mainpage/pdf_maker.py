3269ea1356f6c14a890b2f4839e49c21af0cd26f
alexfaraday <like.alex.melnikov@gmail.com>
2023-05-27 22:45:44 +0300

    correct models

--- mainpage/pdf_maker.py
+++ mainpage/pdf_maker.py
@@ -20,7 +20,7 @@ import os
 #
 def image_3(path='/home/c/cp31594/django_gsvno/public_html/media/img/3.jpg'): #Здесь данные для 3 страницы
     im = Image.open(path)
-    font = ImageFont.truetype("/home/c/cp31594/django_gsvno/public_html/media/fonts/Roboto-Regular.ttf", 64, encoding='UTF-8')
+    font = ImageFont.truetype("/home/c/cp31594/django_gsvno/public_html/media/fonts/Roboto-Regular.ttf", 64,layout_engine=ImageFont.LAYOUT_BASIC, encoding='UTF-8')
     draw_text = ImageDraw.Draw(im)
 
     branch = "Авиационная промышленность"
@@ -59,7 +59,7 @@ def image_3(path='/home/c/cp31594/django_gsvno/public_html/media/img/3.jpg'): #�
 
 def image_4(path='/home/c/cp31594/django_gsvno/public_html/media/img/4.jpg'): #Здесь данные для 4 страницы
     im = Image.open(path)
-    font = ImageFont.truetype("/home/c/cp31594/django_gsvno/public_html/media/fonts/Roboto-Regular.ttf", 64, encoding='UTF-8')
+    font = ImageFont.truetype("/home/c/cp31594/django_gsvno/public_html/media/fonts/Roboto-Regular.ttf", 64,layout_engine=ImageFont.LAYOUT_BASIC, encoding='UTF-8')
     draw_text = ImageDraw.Draw(im)
 
     min_totals = 100
