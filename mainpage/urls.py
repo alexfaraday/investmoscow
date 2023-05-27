08fad38b60a556b697a9de0a6c9d576e9d53226e
alexfaraday <like.alex.melnikov@gmail.com>
2023-05-27 09:56:13 +0300

    correct models

--- mainpage/urls.py
+++ mainpage/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('', registration, name="register"),
     path('main/calculator/', CalculatorView.as_view(), name="calculator"),
     #path('main/<int:pk>/update/', ProfileUpdateView.as_view(), name="profile_update"),
-    path('main/<int:pk>/update/', views.update_profile),
+    path('editprofile/', views.update_profile),
     path('main/<int:pk>/', ProfileDetailView.as_view(), name="profile"),
 
     path('logout/', MyLogoutView.as_view(), name="logout"),
