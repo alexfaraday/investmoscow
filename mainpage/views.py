a6ede53ebb70b96f12c4e787331f2240a8eda6ce
alexfaraday <like.alex.melnikov@gmail.com>
2023-05-28 13:56:00 +0300

    correct models

--- mainpage/views.py
+++ mainpage/views.py
@@ -70,7 +70,7 @@ class CalculatorView(CreateView):
         obj.pdf_link=make_invest_pdf()
         string=str(CalculatorForm.cleaned_data['area_type']).replace('>', '').split(":")
 
-        building_rent=rent(CalculatorForm.cleaned_data['area_yardage'],string[1],1,False)
+        building_rent=rent(CalculatorForm.cleaned_data['area_yardage'],string,1,False)
         items = list(building_rent.items())
 
         obj.building_sum_min=items[0][1]
